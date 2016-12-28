8d8197212b08358ac6ef7f349a4d7b4cbcf0f6e8
Kevinschmid86 <Kevin_Jaeck@web.de>
2016-12-28 20:50:45 +0100

    Initial commit

---
+++ KSchmidSmartyPlugins/KSchmidSmartyPlugins.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kevin.schmid
+ * Date: 06.11.2016
+ * Time: 12:03
+ */
+
+
+namespace KSchmidSmartyPlugins;
+
+use Enlight_Event_EventArgs;
+use Shopware\Components\Plugin;
+
+class KSchmidSmartyPlugins extends Plugin
+{
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            'Enlight_Controller_Action_PostDispatchSecure' => 'onFrontendPostDispatchSecure'
+        ];
+    }
+ 
+    /**
+     * @param Enlight_Event_EventArgs $arguments
+     */
+    public function onFrontendPostDispatchSecure(Enlight_Event_EventArgs $arguments)
+    {
+        $template = $this->container->get('Template');
+        $template->addPluginsDir($this->getPath() . '/smarty/plugins/');
+    }
+
+}
